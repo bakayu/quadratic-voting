aec3fce5d25333392acfa07a24600825d8471d4c
Ayush <mail@ayuch.dev>
2026-02-11 02:17:28 +0530

    feat: add quadratic voting program instructions
    Signed-off-by: Ayush <mail@ayuch.dev>

--- programs/quadratic-voting/src/lib.rs
+++ programs/quadratic-voting/src/lib.rs
@@ -3,17 +3,23 @@ use anchor_lang::prelude::*;
 pub mod instructions;
 pub mod state;
 
+use instructions::*;
+
 declare_id!("78ncauiBZU5AdQ2SAVwFiDuhUvFmRyuF2Pa6QgwA1B6k");
 
 #[program]
 pub mod quadratic_voting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn init_dao(ctx: Context<InitDao>, name: String) -> Result<()> {
+        instructions::init_dao(ctx, name)
+    }
+
+    pub fn init_proposal(ctx: Context<InitProposalContext>, metadata: String) -> Result<()> {
+        instructions::init_proposal(ctx, metadata)
+    }
+
+    pub fn cast_vote(ctx: Context<CastVote>, vote_type: u8) -> Result<()> {
+        instructions::cast_vote(ctx, vote_type)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
